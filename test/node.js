02afcaa1fd784f3e0642a7d94fbb22d72f368f89
Elijah Insua <tmpvar@gmail.com>
2011-02-05 01:41:51 -0500

    more test infrastructure

--- test/node.js
+++ test/node.js
@@ -1,8 +1,30 @@
-var tests = module.exports = {};
+var tests      = module.exports = {};
+    jsdom      = require("jsdom"),
+    fs         = require("fs"),
+    html       = function(file, cb) {
+      fs.readFile(file, function(err, data) {
+        if (err) {
+          return cb(err);
+        }
+
+        var window = jsdom.html(data.toString()).createWindow();
+        jsdom.jQueryify(window, __dirname + "/jquery.js", function() {
+          cb(null, window);
+        });
+      })
+    };
 
 /*
-
-
+  file
+  selector
+  map
+  data
 */
 
 
+tests.template_singular_instance = function(t) {
+  html(__dirname + "/files/singular.html", function(err, window) {
+    
+    t.done()
+  });
+}
