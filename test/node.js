42370bf75b154adfaad3fa612f7230aecaa4db56
hij1nx <fernmicro@gmail.com>
2011-02-05 02:33:12 -0500

    complete and total hardcore refactor action

--- test/node.js
+++ test/node.js
@@ -1,7 +1,7 @@
 var tests      = module.exports = {};
     jsdom      = require("jsdom"),
     fs         = require("fs"),
-    weld       = require("../lib/weld"),
+    weld       = require("../lib/weld").weld,
     html       = function(file, cb) {
       fs.readFile(file, function(err, data) {
         if (err) {
@@ -27,21 +27,10 @@ tests.template_singular_instance = function(t) {
   html(__dirname + "/files/singular.html", function(err, window) {
     
     var dummyData1 = [{ "key": "someKey", "value": "someValue" }];
-    var dummyData2 = [{ "key": "fllasdasd", "value": "Asdase" }];    
     
-    weld('#singular', dummyData1);
-    weld('#other', dummyData2);
-
-    // ...
-
-    weld('#foo', "/foo.html", function(node) {});
-
-    // ...
-
-    weld('#singular', dummyData3, function() {});
+    weld('#singular', dummyData1);    
     
-    
-    t.done()
+    t.done();
   });
 }
 
