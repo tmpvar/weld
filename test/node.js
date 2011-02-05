655b85b083b49367617b1d2cbf98e22234d50463
hij1nx <fernmicro@gmail.com>
2011-02-05 02:30:11 -0500

    complete and total hardcore refactor action

--- test/node.js
+++ test/node.js
@@ -27,12 +27,19 @@ tests.template_singular_instance = function(t) {
   html(__dirname + "/files/singular.html", function(err, window) {
     
     var dummyData1 = [{ "key": "someKey", "value": "someValue" }];
+    var dummyData2 = [{ "key": "fllasdasd", "value": "Asdase" }];    
     
-    weld({
+    weld('#singular', dummyData1);
+    weld('#other', dummyData2);
 
-      '#singular': { data: dummyData1 }
+    // ...
 
-    });
+    weld('#foo', "/foo.html", function(node) {});
+
+    // ...
+
+    weld('#singular', dummyData3, function() {});
+    
     
     t.done()
   });
