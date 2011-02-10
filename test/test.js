335f545a9edfe433b143c83b33b3b578d5283d2a
Elijah Insua <tmpvar@gmail.com>
2011-02-09 23:48:22 -0500

    added custom insertion method and test

--- test/test.js
+++ test/test.js
@@ -14,10 +14,10 @@ var tests      = module.exports = {}
         jsdom.jQueryify(window, __dirname + '/../demo/public/js/jquery.js', function() {
           // remove the jQuery script tag
           window.$('script:last').remove();
-          
+
           // TODO: this is nasty, but quick.
           var weldTag = window.document.createElement('script');
-          
+
           weldTag.src = 'file://' + __dirname + '/../lib/weld.js';
           weldTag.onload = function() {
             // remove the weld scripttag
@@ -32,7 +32,13 @@ var tests      = module.exports = {}
 tests.template_singular_instance = function(t) {
   html('singular.html', function(err, weld, $, window) {
 
-    var data = { key: 'someKey', value: 'someValue', icon : '/path/to/image.png' }; // some dummy data that could come from
+     // some dummy data
+    var data = {
+      key   : 'someKey',
+      value : 'someValue',
+      icon  : '/path/to/image.png'
+    };
+
     weld('#singular', data);
     t.ok($('.key').html() === data.key);
     t.ok($('.icon').attr('src') === data.icon);
@@ -45,7 +51,7 @@ tests.template_alternate_insertion_with_bind = function(t) {
 
     var data = [{ name: 'Paulo',  title: 'code exploder' },
                 { name: 'Elijah', title: 'code pimp' }];
-    
+
     weld('.contact', data, { bind: { 'name': '.foo', 'title': '.title' } });
     t.ok($('.contact .foo:first').text().length > 1);
     t.done();
@@ -57,26 +63,43 @@ tests.template_alternate_insertion_method = function(t) {
 
     var data = [{ name: 'Paulo',  title : 'code exploder' },
                 { name: 'Elijah', title : 'code pimp' }];
-    
+
     weld('.contact', data);
     weld('.contact', data, { method: "prepend" });
-    
     t.ok($('.contact:first .name').text() == "Elijah");
     t.done();
   });
 };
 
+tests.template_custom_insertion_method = function(t) {
+  html('contacts.html', function(err, weld, $, window) {
 
+    var data = [{ name: 'Paulo',  title : 'code master' },
+                { name: 'Elijah', title : 'code pimp' }];
+
+var times = 1;
+    weld('.contact', data, {
+     method: function(parent, newElement) {
+     times++;
+       parent.prepend(newElement);
+     }
+    });
+
+    t.ok($('.contact').length == 2);
+    t.ok($('.contact:nth(0) .name').text() == "Elijah");
+    t.done();
+  });
+};
 
 tests.template_append = function(t) {
   html('contacts.html', function(err, weld, $, window) {
 
-    var data = [{ name: 'Paulo',  title : 'code exploder' },
+    var data = [{ name: 'Paulo',  title : 'code master' },
                 { name: 'Elijah', title : 'code pimp' }];
-    
+
     weld('.contact', data);
     weld('.contact', data);
-    
+
     t.ok($('.contact .name').length > 2);
     t.done();
   });
@@ -85,7 +108,7 @@ tests.template_append = function(t) {
 
 tests.template_array_of_instances = function(t) {
   html('contacts.html', function(err, weld, $, window) {
-    
+
     var data = [{ name: 'Paulo',  title : 'code exploder' },
                 { name: 'Elijah', title : 'code pimp' }];
 
@@ -120,14 +143,14 @@ tests.template_nested_objects = function(t) {
     });
 
     t.ok($('.person').length === 2);
-    /*
-      Every node that gets iterated over should have a pre-processed class
-      (7 in total)
-    */
+
+    //  Every node that gets iterated over should have a pre-processed class
+    // (7 in total)
+
     t.ok($('.pre-processed').length === 7);
     t.done();
   });
-}
+};
 
 tests.template_form_elements = function(t) {
   html('form.html', function(err, weld, $, window) {
@@ -141,4 +164,4 @@ tests.template_form_elements = function(t) {
     t.done();
 
   });
-}
+};
