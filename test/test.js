92019d3d1265fafaeb0a113316649715ae94e084
Elijah Insua <tmpvar@gmail.com>
2011-02-26 14:03:55 -0500

    removed dupe test #6 and re-numbered

--- test/test.js
+++ test/test.js
@@ -137,14 +137,16 @@ module.exports = {
 
     },
     function(errors, window) {
-    
-      var $ = window.jQuery;
 
-      var data = [{ name: 'hij1nx',  title: 'code exploder' },
-                  { name: 'tmpvar', title: 'code pimp' }];
+      var
+      $     = window.jQuery,
+      data  = [{ name: 'hij1nx',  title: 'code exploder' },
+               { name: 'tmpvar', title: 'code pimp' }],
+      times = 0;
 
       window.weld($('.contact')[0], data, {
          insert: function(parent, element) {
+           times++;
            parent.insertBefore(element, parent.firstChild);
          }
       });
@@ -156,45 +158,14 @@ module.exports = {
 
       test.ok($('.contact:nth(0) .title').text() == "code pimp");
       test.ok($('.contact:nth(1) .title').text() == "code exploder");
+      
+      test.ok(times === 2);
       test.done();
     });
 
   },
 
-  "Test 6: Generate markup based on an element using a custom data mapping method": function(test) {
-  
-    jsdom.env({
-
-      scripts: [jqpath, wpath],
-      html: path.join(__dirname, 'files', 'contacts.html')
-
-    },
-    function(errors, window) {  
-    
-      var $ = window.jQuery;    
-
-      var times = 1;
-      var data = [{ name: 'hij1nx',  title : 'code master' },
-                  { name: 'tmpvar', title : 'code pimp' }];
-
-      window.weld($('.contact')[0], data, {
-      
-        insert: function(parent, newElement) {
-          times++;
-          $(parent).prepend(newElement);
-        }
-      
-      });
-
-      test.ok($('.contact').length == 2);
-      test.ok($('.contact:nth(0) .name').text() == "tmpvar");
-      test.done();
-
-    });
-
-  },  
-  
-  "Test 7: Append to a node that has already been the subject of a weld": function(test) {
+  "Test 6: Append to a node that has already been the subject of a weld": function(test) {
   
     jsdom.env({
 
@@ -231,7 +202,7 @@ module.exports = {
   
   },
   
-  "Test 8: Create markup from an array of objects that have one dimention": function(test) {
+  "Test 7: Create markup from an array of objects that have one dimention": function(test) {
 
     jsdom.env({
 
@@ -260,7 +231,7 @@ module.exports = {
 
   },
   
-  "Test 9: Try to pair data with selectors that yield no matching elements": function(test) {
+  "Test 8: Try to pair data with selectors that yield no matching elements": function(test) {
 
     jsdom.env({
 
@@ -291,7 +262,7 @@ module.exports = {
 
   },
 
-  "Test 10: Create markup from an object literal that has one dimention that contains are array of objects with one dimention": function(test) {
+  "Test 9: Create markup from an object literal that has one dimention that contains are array of objects with one dimention": function(test) {
 
     jsdom.env({
 
@@ -299,7 +270,7 @@ module.exports = {
       html: path.join(__dirname, 'files', 'array-of-arrays.html')
 
     },
-    function(errors, window) {  
+    function(errors, window) {
 
       var $ = window.jQuery;
       // TODO: remove siblings who match .person
@@ -353,76 +324,60 @@ module.exports = {
       // (7 in total)
       //test.ok($('.pre-processed').length === 10);
       test.done();
-      
+
     });
 
   },
    
-   "Test 11: Create markup using form elements as the template": function(test) {
+ "Test 10: Create markup using form elements as the template": function(test) {
 
-      jsdom.env({
+    jsdom.env({
 
-        scripts: [jqpath, wpath],
-        html: path.join(__dirname, 'files', 'form.html')
+      scripts: [jqpath, wpath],
+      html: path.join(__dirname, 'files', 'form.html')
 
-      },
-      function(errors, window) {
+    },
+    function(errors, window) {
 
+      var $ = window.jQuery;
+
+      var data = {
+        'email' : 'tmpvar@gmail.com'
+      };
+
+      window.weld($('form')[0], data);
+
+      test.ok($(':input[name=email]').val() === data.email);
+      test.done();
+
+    });
+  },
+  "Test 11: Returning false from map stops the current branch from being visited" : function(test) {
+    jsdom.env('<ul class="list"><li class="item">hello <span class="where">do not touch</span></li></ul>',[
+      jqpath, wpath
+    ], function(errors, window) {
+      
+      var $ = window.jQuery;
+      
+      window.weld($('.list .item')[0], [
+        { where : 'world' }
+      ], {
+        map : function(element, k, v) {
+          return false;
+        }
+      });
+
+      test.ok($('.where').text() === 'do not touch');
+      test.done();
+
+    });
+  },
+  "Test 12: Use a NodeList from another document and weld it into the target document" : function(test) {
+    jsdom.env(path.join(__dirname, 'files', 'source.html'), function(serrs, sw) {
+      var sources = sw.document.getElementsByTagName("span");
+
+      jsdom.env(path.join(__dirname, 'files', 'dest.html'),[jqpath, wpath], function(errors, window) {
         var $ = window.jQuery;
-
-        var data = {
-          'email' : 'tmpvar@gmail.com'
-        };
-
-        window.weld($('form')[0], data);
-
-        test.ok($(':input[name=email]').val() === data.email);
-        test.done();
-
-      });
-    },
-    "Test 12: Returning false from map stops the current branch from being visited" : function(test) {
-      jsdom.env('<ul class="list"><li class="item">hello <span class="where">do not touch</span></li></ul>',[
-        jqpath, wpath
-      ], function(errors, window) {
-        
-        var $ = window.jQuery;
-        
-        window.weld($('.list .item')[0], [
-          { where : 'world' }
-        ], {
-          map : function(element, k, v) {
-            return false;
-          }
-        });
-
-        test.ok($('.where').text() === 'do not touch');
-        test.done();
-        
-      });
-    },
-    "Test 13: Use a NodeList from another document and weld it into the target document" : function(test) {
-      jsdom.env(path.join(__dirname, 'files', 'source.html'), function(serrs, sw) {
-        var sources = sw.document.getElementsByTagName("span");
-
-        jsdom.env(path.join(__dirname, 'files', 'dest.html'),[jqpath, wpath], function(errors, window) {
-          var $ = window.jQuery;
-
-          window.weld($('li.number')[0], sources);
-
-          test.ok($('li.number').length === 3);
-          test.ok($('li.number:nth(0) span').text() === "zero");
-          test.ok($('li.number:nth(1) span').text() === "one");
-          test.ok($('li.number:nth(2) span').text() === "two");
-          test.ok($('li.number').text() === "zeroonetwo");
-          test.done();
-        });
-      });
-    },
-    "Test 14: Use a NodeList from the current document and weld it to another area in the same document" : function(test) {
-      jsdom.env(path.join(__dirname, 'files', 'source-and-dest.html'),[jqpath, wpath], function(errors, window) {
-        var $       = window.jQuery,
-            sources = window.document.getElementById('data').getElementsByTagName("span");
 
         window.weld($('li.number')[0], sources);
 
@@ -433,37 +388,55 @@ module.exports = {
         test.ok($('li.number').text() === "zeroonetwo");
         test.done();
       });
-    },
-    "Test 15: Alias may return a dom element which is used instead of doing an explicit match": function(test) {
+    });
+  },
 
-      jsdom.env(path.join(__dirname, 'files', 'contacts-alias.html'), [jqpath, wpath],  function(errors, window) {
+  "Test 13: Use a NodeList from the current document and weld it to another area in the same document" : function(test) {
+    jsdom.env(path.join(__dirname, 'files', 'source-and-dest.html'),[jqpath, wpath], function(errors, window) {
+      var $       = window.jQuery,
+          sources = window.document.getElementById('data').getElementsByTagName("span");
 
-        var $     = window.jQuery,
-            data  = [{ name: 'hij1nx',  title: 'code exploder' },
-                     { name: 'tmpvar', title: 'code pimp' }],
-                    
-            found = 0;
+      window.weld($('li.number')[0], sources);
 
-        window.weld($('.contact')[0], data, { 
-          alias: { 
-            name : function(parent, element, key, value) { 
-              // Sanity
-              test.ok(key === 'name');
+      test.ok($('li.number').length === 3);
+      test.ok($('li.number:nth(0) span').text() === "zero");
+      test.ok($('li.number:nth(1) span').text() === "one");
+      test.ok($('li.number:nth(2) span').text() === "two");
+      test.ok($('li.number').text() === "zeroonetwo");
+      test.done();
+    });
+  },
 
-              return $('.foo', element)[0];
-            }
+  "Test 14: Alias may return a dom element which is used instead of doing an explicit match": function(test) {
+
+    jsdom.env(path.join(__dirname, 'files', 'contacts-alias.html'), [jqpath, wpath],  function(errors, window) {
+
+      var $     = window.jQuery,
+          data  = [{ name: 'hij1nx',  title: 'code exploder' },
+                   { name: 'tmpvar', title: 'code pimp' }],
+                  
+          found = 0;
+
+      window.weld($('.contact')[0], data, { 
+        alias: { 
+          name : function(parent, element, key, value) { 
+            // Sanity
+            test.ok(key === 'name');
+
+            return $('.foo', element)[0];
           }
-        });
-
-        test.ok($('.contact').length === 2);
-
-        test.ok($('.contact:nth(0) .foo').text() == "hij1nx");
-        test.ok($('.contact:nth(1) .foo').text() == "tmpvar");
-
-        test.ok($('.contact:nth(0) .title').text() == "code exploder");
-        test.ok($('.contact:nth(1) .title').text() == "code pimp");
-
-        test.done();
+        }
       });
-    },
+
+      test.ok($('.contact').length === 2);
+
+      test.ok($('.contact:nth(0) .foo').text() == "hij1nx");
+      test.ok($('.contact:nth(1) .foo').text() == "tmpvar");
+
+      test.ok($('.contact:nth(0) .title').text() == "code exploder");
+      test.ok($('.contact:nth(1) .title').text() == "code pimp");
+
+      test.done();
+    });
+  },
 };
